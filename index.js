691c46887a8d574d98b5b218951b9b71ef135f8f
yuchangchang <yuchangchang@hd123.com>
2021-05-28 15:46:39 +0800

    1

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: 余畅畅
+ * @Date: 2021-05-27 11:28:43
+ * @LastEditTime: 2021-05-27 11:30:34
+ * @LastEditors: 余畅畅
+ * @Description:
+ * @FilePath: \mymomentjs\index.js
+ *
+ */
+import calendar from './src/Calendar'
+import DateUtil from './src/DateUtil'
+
+export { calendar, DateUtil }
